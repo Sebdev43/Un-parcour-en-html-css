6ad3249b80f080e89c01b26c539a25ae5ab5b761
Sebdev43 <sebastien.robert@oclock.school>
2024-08-05 09:03:43 +0200

    style: :lipstick: css

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,23 @@
 import { defineConfig } from 'vite'
 import viteCompression from 'vite-plugin-compression'
 import { visualizer } from 'rollup-plugin-visualizer'
+import mime from 'mime-types'
 
 export default defineConfig({
   plugins: [
     viteCompression(),  // Compression des fichiers
-    visualizer()        // Analyse des bundles
+    visualizer(),       // Analyse des bundles
+    {
+      name: 'configure-mime',
+      configureServer(server) {
+        server.middlewares.use((req, res, next) => {
+          if (req.url.endsWith('.css')) {
+            res.setHeader('Content-Type', mime.lookup('css'));
+          }
+          next();
+        });
+      },
+    },
   ],
   build: {
     terserOptions: {
